3ef4d55281b7388217a06260567a77baedd2d2df
Shane Loretz <sloretz@osrfoundation.org>
2021-08-11 17:00:50 -0700

    Everything is still broken, but progress
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- siws/create.py
+++ siws/create.py
@@ -36,36 +36,20 @@ def create_workspace(name, ws_path, from_, binds):
         raise RuntimeError(f'Cannot create container here because {siws_folder} already exists')  # noqa
 
     container_folder = container_path(siws_folder, name)
+    build_commands = []
 
-    main_template_args = {
-        'siws_version': __version__,
-        'workspace_path': str(ws_path.resolve()),
-        'container_paths': [str(container_folder.resolve())],
-        'comment_build_commands': [],
-        'binds': [],
-        'container_commands': None
-    }
-    main_template = files('siws.templates').joinpath('siws.in')
-    # Commands template arguments
-    #   container_name
-    commands_template = files('siws.templates').joinpath('container_commands.in')  # noqa
-    # Paths template arguments
-    #   container_name
-    #   container_path
-    paths_template = files('siws.templates').joinpath('container_paths.in')
-
-    sandbox_path = str(ws_path.joinpath(name + '.sandbox'))
+    sandbox_path = str(siws_folder.joinpath(name + '.sandbox'))
 
     build_command = [
         'singularity',
         'build',
         '--fakeroot',
         '--sandbox',
-        sandbox_path,
+        str(container_folder.resolve()),
         from_
     ]
     print(pretty_cmd(build_command))
-    main_template_args['comment_build_commands'].append(build_command)
+    build_commands.append(build_command)
     # TODO(sloretz) uncomment this when ready :)
     # TODO(sloretz) pick better log output spot
     # execute_and_log(build_command, '/tmp/' + name)
@@ -89,23 +73,42 @@ def create_workspace(name, ws_path, from_, binds):
             # TODO(sloretz) what does singularity do with relative paths in binds?
             destination
         ]
-        main_template_args['comment_build_commands'].append(exec_command)
+        build_commands.append(exec_command)
         print(pretty_cmd(exec_command))
         # TODO(sloretz) uncomment when ready :)
         # subprocess.run(exec_command, check=True)
-        main_template_args['binds'].append(str(bind))
 
-    # Now write a .siws file!
-    if main_template_args['binds']:
-        main_template_args['binds'] = ','.join(main_template_args['binds'])
-        main_template_args['binds'] = '--bind ' + main_template_args['binds']
+    # Now write the config file!
 
-    commands = []
-    for cmd in main_template_args['comment_build_commands']:
-        commands.append("#   " + pretty_cmd(cmd))
-    main_template_args['comment_build_commands'] = "\n".join(commands)
+    tmp_commands = []
+    for cmd in build_commands:
+        tmp_commands.append("#   " + pretty_cmd(cmd))
+    build_commands = tmp_commands
+    del tmp_commands
 
+    paths_template_args = {
+        'container_name': name,
+        'container_path': str(container_folder.resolve())
+    }
+    print(paths_template_args)
+    paths_template = Template(files('siws.templates').joinpath('container_paths.in'))  # noqa
+    print(paths_template.substitute(paths_template_args))
+
+    commands_template_args = {
+        'container_name': name
+    }
+    commands_template = Template(files('siws.templates').joinpath('container_commands.in'))  # noqa
+
+    main_template_args = {
+        'siws_version': __version__,
+        'workspace_path': str(ws_path.resolve()),
+        'container_paths': paths_template.substitute(paths_template_args),
+        'comment_build_commands': "\n".join(build_commands),
+        'binds': '--bind ' + ','.join(map(str, binds)),
+        'container_commands': commands_template.substitute(command_template_args)
+    }
     print(main_template_args)
 
-    print(Template(main_template).substitute(main_template_args))
+    main_template = Template(files('siws.templates').joinpath('siws.in'))
+    print(main_template.substitute(main_template_args))
 
